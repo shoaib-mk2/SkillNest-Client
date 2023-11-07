686e671d4138562ae4f764a750b263a902a1d82a
shoaib-mk2 <shoaibchowdhury707@gmail.com>
2023-11-08 00:58:04 +0600

    added navbar section

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../pages/NavBar/NavBar";
+import Footer from "../pages/Footer/Footer";
 
 const Root = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
